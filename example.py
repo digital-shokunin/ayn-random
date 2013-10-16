38a3da4f7a1b69aa98d58501e82663da152f3bef
surge <david.mitchell@digital-shokunin.net>
2013-10-15 20:06:10 -0400

    Ayn Random example

--- example.py
+++ example.py
@@ -5,7 +5,7 @@ __author__ = 'David Mitchell'
 
 for i in range(1, 100):
     number = aynrandom(1, 50000)
-    if number == None:
-        print "NONE: need to find bug"
-    elif genprime.isPrime(number):
+    if genprime.isPrime(number):
         print "Is prime: " + str(number)
+    else:
+        print "NOT PRIME: " + str(number)
